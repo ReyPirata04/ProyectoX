5fc5bed6204e8fe6c4249785e85f2647c2043fa7
Lucas Flores <gian.flores@davinci.edu.ar>
2024-09-16 11:10:25 -0300

    Función de suma

--- hello.py
+++ hello.py
@@ -4,4 +4,13 @@ nombre = input("Ingresa tu nombre: ")
 def saludar(nombre):
     print(f"¡Hola, bienvenido {nombre}")
 
-saludar(nombre)    
+saludar(nombre)
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+def suma(num1. num2):
+    suma=num1+num2
+    print("La suma es:",suma)
+
+suma(num1,num2)
