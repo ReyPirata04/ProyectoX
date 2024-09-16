d87bfe6894fe51145b4c56488d4be5754ec2088d
Lucas Flores <gian.flores@davinci.edu.ar>
2024-09-16 19:34:45 -0300

    Comentarios Agregados

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
 #Mensaje de bienvenida
 nombre = input("Ingresa tu nombre: ")
 
+#Función de saludar
 def saludar(nombre):
     print(f"¡Hola, bienvenido {nombre}")
 
 saludar(nombre)
 
+#Suma de 2 numeros
 num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
 
+#Función de la suma de numeros
 def suma(num1, num2):
     suma=num1+num2
     print("La suma es:",suma)
