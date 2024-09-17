dc6d456d13a7d4ec1af22846b107620f40b9419c
Lucas Flores <gian.flores@davinci.edu.ar>
2024-09-17 10:53:49 -0300

    Función de resta agregada

--- hello.py
+++ hello.py
@@ -9,8 +9,14 @@ saludar(nombre)
 num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
 
+#Funciones de suma y resta 
 def suma(num1, num2):
     suma=num1+num2
     print("La suma es:",suma)
 
+def resta(num1, num2):
+    resta=num1-num2
+    print("La resta es:",resta)
+
 suma(num1,num2)
+resta(num1, num2)
